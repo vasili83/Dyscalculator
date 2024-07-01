fa21a1f3814c969a06a04df0983e616f242196d1
vasili83 <vasili.paspalas@gmail.com>
2024-07-01 17:42:56 +0200

    compare and select cheapest product option

--- repos/src/components/compare-product.vue
+++ repos/src/components/compare-product.vue
@@ -1,17 +1,21 @@
 <template>
-  <q-card class="q-my-sm">
+  <q-card class="q-ma-sm">
     <q-card-section class="input-container">
       <div class="input-field">
         <q-select filled v-model="measureOptionChosen" :options="measureOptions" label="Maat">
-          <template v-slot:prepend>
-            <q-icon name="scale" />
-          </template>
         </q-select>
       </div>
       <div class="input-field">
-        <q-input v-model="amount" :label="`${capitalize(measureType)} in ${measureOptionChosen.label}`" type="number" stack-label>
+        <q-input 
+          v-model="amount" 
+          :label="`${capitalize(productData.type)} in ${measureOptionChosen ? measureOptionChosen.label : ''}`" 
+          type="number" 
+          stack-label>
+          <template v-slot:prepend>
+            <q-icon :name="(productData.type === 'gewicht' ? 'scale' : 'local_drink')" />
+          </template>
           <template v-slot:append>
-            <q-icon name="close" @click="amount = null" class="cursor-pointer" />
+            <q-icon name="clear" @click="amount = null" class="cursor-pointer" />
           </template>
         </q-input>
       </div>
@@ -20,73 +24,66 @@
           <template v-slot:prepend>
             <q-icon name="euro" />
           </template>
+          <template v-slot:append>
+            <q-icon name="clear" @click="amount = null" class="cursor-pointer" />
+          </template>
         </q-input>
       </div>
+    </q-card-section>
+    <q-card-section>
+    <template v-if="productData.normalizedPrice > 0">
+      € {{ productData.normalizedPrice }} per {{ ( productData.type === 'gewicht' ? 'Kg' : 'Ltr') }}
+    </template>
+    <template v-else>
+      <q-banner dense class="bg-gray rounded-borders">
+        Voer {{ productData.type }} en prijs in.
+      </q-banner>
+    </template>
     </q-card-section>
   </q-card>
 </template>
 
 <script setup>
-// ## IMPORTS
-import { ref, watch } from 'vue';
+import { ref, watch, computed } from 'vue';
+const props = defineProps(['product', 'index']);
+const emits = defineEmits(['productData']);
+const price = ref(0);
+const amount = ref(0);
 
-// ## INITS
-const props = defineProps(['type']);
-const price = ref(null);
-const amount = ref(null);
-
-// ## DEFINITIONS
-const measurements = 
-{   'weight': [
-  {
-    label: 'gram (gr)',
-    value: 'gr',
-    calc: 1000
-  },
-  {
-    label: 'kilo (kg)',
-    value: 'kg',
-    calc: 0
-  },
-],
-'volume': [
-  {
-    label: 'mililiter (ml)',
-    value: 'ml',
-    calc: 1000
-  },
-  {
-    label: 'centiliter (cl)',
-    value: 'cl',
-    calc: 100
-  },
-  {
-    label: 'liter (ltr)',
-    value: 'ltr',
-    calc: 0
-  }
-]};
-let measureType = props.type;
-const measureOptions = ref(measurements[measureType]);
+const measureOptions = ref(props.product.measureOptions);
 const measureOptionChosen = ref(measureOptions.value[0]);
 
-// ## RUNTIME CYCLES / GUARDS
-watch(()=>props.type, ()=>{
-  measureType = props.type;
-  measureOptions.value = measurements[measureType];
+watch(() => props.product.type, () => {
+  measureOptions.value = props.product.measureOptions;
   measureOptionChosen.value = measureOptions.value[0];
+  // amount.value = 0;
+  // price.value = 0;
 });
 
-// ## FUNCTIONS
+const productData = computed(()=>{
+  let pd = {
+    index: props.index,
+    type: props.product.type,
+    measure: measureOptionChosen.value,
+    amount: parseFloat(amount.value),
+    price: parseFloat(price.value),
+  }
+  pd['normalizedPrice'] = parseFloat(((pd.price / pd.amount) * pd.measure.calc).toFixed(2));
+  return pd
+});
+
+
+watch([amount, price], () => {
+  emits('productData', productData);
+});
+
 const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
-
-
 </script>
 
 <style scoped>
 .input-container {
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
 }
 
 .input-field {
--- repos/src/pages/IndexPage.vue
+++ repos/src/pages/IndexPage.vue
@@ -1,31 +1,119 @@
 <template>
-  <q-page class="row items-center justify-center">
-    <div class="compare-products">
+  <q-page>
     <q-tabs v-model="type" class="text-teal">
-    <q-tab name="weight" class="weight-tab" icon="scale" label="Gewicht (Kg, gr)" />
-    <q-tab name="volume" class="volume-tab" icon="straighten" label="Volume (ltr, ml)" />
-  </q-tabs>
-    <CompareProduct :type />
-    <CompareProduct :type />
-    <CompareProduct :type />
-  </div>
+      <q-tab name="gewicht" class="weight-tab" icon="scale" label="Gewicht (Kg, gr)" />
+      <q-tab name="volume" class="volume-tab" icon="straighten" label="Volume (ltr, ml)" />
+    </q-tabs>
+    <div class="compare-products justify-center">
+      <CompareProduct v-for="(product, key) in compareData" :key :index="key" :product @productData="updateCompareData"
+        :class="`product ${key === getCheapestProduct ? 'cheapest' : ''}`" />
+    </div>
+    <q-btn-group unelevated class="buttons">
+      <q-btn rounded flat icon="add" @click="addComparingProduct" color="primary" label="voeg toe" />
+      <q-btn rounded flat icon="delete" @click="removeComparingProduct" color="pink" label="verwijder" />
+    </q-btn-group>
+    <!-- <q-page-sticky position="bottom-right" :offset="[18, 18]">
+            <q-btn fab icon="add" color="primary" />
+    </q-page-sticky> -->
   </q-page>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import CompareProduct from '../components/compare-product.vue';
-import { ref } from 'vue';
+import { ref, computed, watch } from 'vue';
 
-let type = ref('weight');
+const measurements = {
+  gewicht: [
+    { label: 'gram (gr)', value: 'gr', calc: 1000 },
+    { label: 'kilo (kg)', value: 'kg', calc: 1 },
+  ],
+  volume: [
+    { label: 'mililiter (ml)', value: 'ml', calc: 1000 },
+    { label: 'centiliter (cl)', value: 'cl', calc: 100 },
+    { label: 'liter (ltr)', value: 'ltr', calc: 1 },
+  ],
+};
+const type = ref('gewicht');
+const measurementByType = computed(() => measurements[type.value]);
+
+const productDataTemplate = {
+  type: type.value,
+  measureOptions: measurementByType.value,
+  measure: measurementByType.value[0],
+  amount: 0,
+  price: 0,
+  normalizedPrice: 0
+};
+
+watch(() => type.value, () => {
+  productDataTemplate.measureOptions = measurementByType.value;
+  productDataTemplate.measure = measurementByType.value[0];
+  productDataTemplate.type = type.value;
+  compareData.value = compareData.value.map(()=>({
+    type: productDataTemplate.type,
+    measureOptions: productDataTemplate.measureOptions,
+    measure: productDataTemplate.measure,
+  }));
+});
+
+const compareData = ref([]);
+
+const addComparingProduct = () => {
+  compareData.value.push({...productDataTemplate});
+}
+const removeComparingProduct = () => {
+  compareData.value.pop();
+}
+const updateCompareData = (productData) => {
+  let index = parseInt(productData.value.index);
+  compareData.value[index] = productData.value;
+}
+
+const getCheapestProduct = computed(() => {
+  let cheapest = compareData.value.reduce((prevResult, current) => {
+    return prevResult.normalizedPrice <= current.normalizedPrice ? prevResult : current
+  })
+  return cheapest.index;
+  // return 1;
+})
+
+addComparingProduct();
+addComparingProduct();
+
 
 </script>
 
 <style scoped>
-.compare-products {
+.page {
   display: flex;
   flex-direction: column;
 }
-:deep(.volume-tab i){
+
+.compare-products {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.product {
+  flex-shrink: 0;
+  border: 3px solid transparent;
+  -webkit-transition: border 500ms ease-out;
+  -moz-transition: border 500ms ease-out;
+  -o-transition: border 500ms ease-out;
+  transition: border 500ms ease-out;
+}
+
+.product.cheapest {
+  border: 3px solid green;
+}
+
+.buttons {
+  width: 100%;
+  justify-content: center;
+}
+
+:deep(.volume-tab i) {
   transform: rotate(-90deg);
 }
 </style>
